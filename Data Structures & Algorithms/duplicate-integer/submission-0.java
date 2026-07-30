f4fd9a3b00f4f1f578f07de452cf77366fae2476
Kumar Sanu <ksanu.37@gmail.com>
2026-07-30 09:01:12 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for(int i : nums) {
+            if(numSet.contains(i)){
+                return true;
+            }
+            numSet.add(i);
+        }
+
+        return false;
+    }
+}
